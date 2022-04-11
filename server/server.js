79c605f8340182814773a47fb9f33ee5820552e8
Mikayla Bruce <mkla.w.b@gmail.com>
2022-04-11 15:35:37 -0700

    deploy

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+console.log(process.env)
+
 const express = require("express");
 
 const { ApolloServer } = require("apollo-server-express");
@@ -26,7 +28,7 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
